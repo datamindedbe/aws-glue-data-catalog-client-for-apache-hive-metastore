ad90aec2a95366cdfc84ceabbc09c891c89a5a64
Stijn De Haes <stijndehaes@gmail.com>
2020-04-28 15:59:04 +0200

    Remove spark shims since spark 3 uses hive2
    Also upgraded to hive2 2.3.6

--- aws-glue-datacatalog-hive2-client/src/main/java/com/amazonaws/glue/catalog/metastore/AWSCatalogMetastoreClient.java
+++ aws-glue-datacatalog-hive2-client/src/main/java/com/amazonaws/glue/catalog/metastore/AWSCatalogMetastoreClient.java
@@ -89,6 +89,8 @@ import org.apache.hadoop.hive.metastore.api.UnknownTableException;
 import org.apache.hadoop.hive.metastore.api.hive_metastoreConstants;
 import org.apache.hadoop.hive.metastore.api.CompactionResponse;
 import org.apache.hadoop.hive.metastore.partition.spec.PartitionSpecProxy;
+import org.apache.hadoop.hive.metastore.api.PartitionValuesRequest;
+import org.apache.hadoop.hive.metastore.api.PartitionValuesResponse;
 import org.apache.log4j.Logger;
 import org.apache.thrift.TException;
 
@@ -1778,6 +1780,12 @@ public class AWSCatalogMetastoreClient implements IMetaStoreClient {
 
     return new Path(currentUri.getScheme(), currentUri.getAuthority(),
           defaultNewPath.toUri().getPath());
+  }
+
+  @Override
+  public PartitionValuesResponse listPartitionValues(PartitionValuesRequest arg0)
+          throws MetaException, TException, NoSuchObjectException {
+    return null;
   }
 
 }
